52d158b7e0c3c3904076cac5781695b80f288fd4
Vaibhav <iamvaibhav2000@gmail.com>
2022-11-15 02:07:25 +0530

    Reversed an array

---
+++ ReverseAnArray.java
@@ -0,0 +1,28 @@
+public class ReverseAnArray {
+
+    static void reversedArray(int[] arr, int start, int end) {
+        int temp;
+        while(start < end) {
+            temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    static void printArray(int[] arr, int size) {
+        for(int i = 0; i < size; i++) {
+           System.out.print(arr[i]);
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6};
+        printArray(arr, 6);
+        reversedArray(arr, 0, 5);
+        System.out.print("Reversed Array 👇\n");
+        printArray(arr, 6);
+    }
+}
